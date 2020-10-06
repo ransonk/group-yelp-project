0e112f70e404b4b9d97fed825ff9bf115db0d991
Wren McPherson <wren.mcpherson@yahoo.com>
2020-10-06 18:53:20 -0400

    worked on restaurants frontend. views and routes updated

--- public/js/restaurant.js
+++ public/js/restaurant.js
@@ -1,7 +1,15 @@
-const fetchRestaurants = () => {
-    return async () => {
-        const res = await fetch('/api/restaurants');
-        const restaurants = res.json();
+// const restaurant = require("../../db/models/restaurant");
+
+const fetchRestaurants = async (input) => {
+    const res = await fetch('/api/restaurants');
+    const { restaurants } = await res.json();
+    if (!input) {
+        return restaurants;
+    } else {
+        // console.log(input);
+        return restaurants.filter(restaurant => {
+            return restaurant.name.toLowerCase().includes(input.toLowerCase());
+        })
     }
 }
 
@@ -10,3 +18,67 @@ const fetchRestaurant = (restaurantId) => {
 
     }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const restaurantsContainer = document.querySelector('.restaurants-container');
+    const search = document.querySelector('.search')
+    search.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        restaurantsContainer.innerHTML = '';
+        const formData = new FormData(search);
+        const input = formData.get('search')
+        try {
+            const results = await fetchRestaurants(input);
+            console.log(results);
+            const restaurantsHTML = results.map(
+                ({ name, address, city, state, phone, Reviews, Images, foodCategory}) => {
+                    let review;
+                    let rating;
+                    if (Reviews.length) {
+                        review = Reviews[0].description;
+                        rating = Reviews.reduce((accum, ele) => {
+                            return accum + ele.rating;
+                        }, 0);
+                        rating /= Reviews.length;
+                        console.log("Review and rating: ", review, rating)
+                    } else {
+                        // set something for review
+                    }
+                    let imgUrl;
+                    if (!Images.length) {
+                        imgUrl = '';
+                    } else {
+                        imgUrl = Images[0].url;
+                    }
+                    // const restaurantDiv = document.createElement('div');
+                    // restaurantDiv.setAttribute('class', 'restaurant-div');
+                    // restaurantDiv.innerHTML = `
+                    console.log(name, address, city, state, phone, Reviews, Images, foodCategory)
+                    return `
+                    <div class='resDiv'>
+                        <img src='${imgUrl}'>
+                        <div class='search__restaurant-name'>
+                        <h2>${name}</h2>
+                        </div>
+                        <div class='search__rating'>
+                        <p>${rating}</p>
+                        </div>
+                        <div class='search__review'>
+                        <p>${review}</p>
+                        </div>
+                        <div class='address'>
+                        <p>${phone}</p>
+                        <p>${address}</p>
+                        <p>${city}, ${state}</p>
+                        </div>
+                    </div>
+                    `
+                })
+                const resHTML = restaurantsHTML.join('');
+                restaurantsContainer.innerHTML += resHTML;
+        }
+        catch (err) {
+    
+        }
+    })
+})
--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,9 @@ router.get("/", (req, res) => {
     res.render('index')
 })
 
+router.get('/search', (req, res) => {
+    res.render('search');
+})
 
 
 
