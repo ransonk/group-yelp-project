869897f2b12fc5db1c18826ea14dd1bc0f859603
Akmal Anvarov <akmalanvaroff@gmail.com>
2020-10-08 02:11:57 +0500

    writing restaurant.js for front end

--- public/js/restaurant.js
+++ public/js/restaurant.js
@@ -1,8 +1,17 @@
-const fetchRestaurant = async (restaurantId) => {
-    const res = await fetch(`/api/restaurants/${restaurantId}`, {
-        headers: {
-            "WWW-Authenticate": `Bearer ${localStorage.getItem("HANGRY_ACCESS_TOKEN")}`
-        }
-    });
-    const { restaurant } = await res.json();
-}
+
+document.addEventListener('DOMContentLoaded', async () => {
+    const restaurantId = window.location.href.match(/\/(\d+)/)[1]
+    try {
+        const res = await fetch(`/api/restaurants/${restaurantId}`, {
+            headers: {
+                "WWW-Authenticate": `Bearer ${localStorage.getItem("HANGRY_ACCESS_TOKEN")}`
+            }
+        });
+        const { restaurant } = await res.json();
+        console.log(restaurant, 'restaurant');
+    } catch(err){
+        console.log(err)
+    }
+    // const restaurant = await fetchRestaurant().json()
+    // fetchRestaurant(3)
+})
