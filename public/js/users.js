e0cbecc8786ab8653910f433563d7a1c33744c89
Wren McPherson <wren.mcpherson@yahoo.com>
2020-10-08 18:03:14 -0400

    made profile page and user reviews

--- public/js/users.js
+++ public/js/users.js
@@ -1,18 +1,37 @@
-const express = require('express');
-const router = express.Router();
 
+document.addEventListener('DOMContentLoaded', async () => {
 
-router.get('/:id(\\d+)', (req, res) => {
+    const usernameDiv = document.querySelector('.profile__user-name');
+    const userPictureDiv = document.querySelector('.profile__user-pic');
+    const reviewsContainer = document.querySelector('.profile__reviews-container');
+
+    const userId = window.location.href.match(/\/(\d+)$/)[1]
+    // console.log(userId)
+    const res = await fetch(`/api/user/${userId}`);
+    const { user: { firstName, lastName, profileUrl }, reviews } = await res.json();
+    console.log(reviews);
+
+    usernameDiv.innerHTML = firstName + ' ' + lastName;
+    userPictureDiv.innerHTML = `<img src="${profileUrl}">`
+    const reviewsArray = reviews.map(({ rating, description, createdAt, Restaurant })=> {
+        const reviewDiv = document.createElement('div');
+        reviewDiv.setAttribute('class', 'profile__review-div');
+        return reviewDiv.innerHTML = `
+            <div class="profile__review-restaurant">
+                <a href="/restaurants/${Restaurant.id}">
+                <h3>${Restaurant.name}</h3>
+                </a>
+            </div>
+            <div class="profile__review-rating">
+                <p>${rating} ${new Date(createdAt).toLocaleString()}</p>
+            </div>
+            <div class="profile__review-description">
+                <p>${description}</p>
+            </div>
+        `
+    })
+    const reviewsHTML = reviewsArray.join('');
+    reviewsContainer.innerHTML += reviewsHTML;
+
 
 })
-
-
-
-
-
-
-
-
-
-
-module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -41,10 +41,13 @@ router.get("/restaurants/:id(\\d+)", (req, res) => {
     res.render("restaurant");
 })
 
-router.get('/restaurants/:id(\\d+)/reviews/new', csrfProtection, (req, res) => {
-    res.render('write-a-review', {csrfToken: req.csrfToken()});
+router.get('/user/:id(\\d+)', (req, res) => {
+    res.render('profile-page');
 })
 
+router.get('/restaurants/:id(\\d+)/reviews/new', (req, res) => {
+    res.render('write-a-review');
+})
 
 
 
