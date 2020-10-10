54467b751d9d342f5e6122de6c08dd39728ce276
Peter <southpeter77@gmail.com>
2020-10-09 23:31:37 -0400

    done with demo button

--- public/js/demo.js
+++ public/js/demo.js
@@ -1,36 +1,23 @@
 document.addEventListener("DOMContentLoaded", (event) => {
+/////////////////database must have a demo user and save the token and hard code in here //////////////
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoyMywiZW1haWwiOiJkZW1vVXNlckBkZW1vLmNvbSJ9LCJpYXQiOjE2MDIzMDAxNDEsImV4cCI6MzAwMDAwMDAxNjAyMzAwMTYwfQ.kEwQrc9WXQue6RQ50KiTna5OGDXNWpOXLJTb88ttwzc"
+const id = 23;
 
-
+localStorage.setItem("HANGRY_ACCESS_TOKEN", token)
+localStorage.setItem("HANGRY_CURRENT_USER_ID", id)
 
     const demoPicture = document.querySelector(".demo__picture");
-
     const demoBubble = document.querySelector(".demo__bubble");
-
-    // demoBubble.classList.remove(".demo__bubble2")
-
     const firstButton = document.querySelector(".next_button1")
     const secondButton = document.querySelector(".next_button2")
-    console.log('loaded')
-
     secondButton.classList.add("hidden")
-
-
-
-
     firstButton.addEventListener("click", (event) => {
         firstButton.classList.add("hidden")
         secondButton.classList.remove("hidden")
         demoBubble.classList.remove("demo__bubble")
         demoBubble.classList.add("demo__bubble2")
-
    })
-    
    secondButton.addEventListener("click", (event) => {
        window.location.href = '/'
    })
-
-
-
-
-
 })
