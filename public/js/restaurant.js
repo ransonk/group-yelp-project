0e115557b4f34099c3dab11c2da79af180faa100
Akmal Anvarov <akmalanvaroff@gmail.com>
2020-10-10 16:09:26 +0500

    restaurant page styles

--- public/js/restaurant.js
+++ public/js/restaurant.js
@@ -16,8 +16,69 @@ const restaurantId = window.location.href.match(/\/(\d+)$/)[1]
 
 document.addEventListener('DOMContentLoaded', async () => {
 
-    const token = localStorage.getItem("HANGRY_ACCESS_TOKEN");
+    // const token = localStorage.getItem("HANGRY_ACCESS_TOKEN");
     const currentUserId = localStorage.getItem("HANGRY_CURRENT_USER_ID");
+    const token = localStorage.getItem("HANGRY_ACCESS_TOKEN")
+    const id = localStorage.getItem("HANGRY_CURRENT_USER_ID")
+
+    const body = { token, id }
+    const res = await fetch("/api/user/check", {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers:
+            { "Content-Type": "application/json" },
+    })
+
+    const loggedInStatus = await res.json();
+
+    document.querySelector('#my-business').addEventListener('click', async (e) => {
+        const res = await fetch(`/api/restaurants/user/${id}/restaurant`);
+
+        const { restaurant } = await res.json();
+        if (!restaurant) {
+
+            window.location.href = "/my-business"
+            // console.log('need to create business page')
+        } else {
+
+            const restaurantId = restaurant.id;
+            window.location.href = `/restaurants/${restaurantId}`;
+        }
+    })
+
+    if (loggedInStatus.result === "Non-business Owner Token") {
+        document.querySelector('#sign-up').classList.add('hidden')
+        document.querySelector('#log-in').classList.add('hidden')
+        document.querySelector('#profile').classList.remove('hidden')
+        document.querySelector('#log-out').classList.remove('hidden')
+        document.querySelector('#my-business').classList.add('hidden')
+    } else if (loggedInStatus.result === "Business Owner Token") {
+        document.querySelector('#sign-up').classList.add('hidden')
+        document.querySelector('#log-in').classList.add('hidden')
+        document.querySelector('#profile').classList.remove('hidden')
+        document.querySelector('#log-out').classList.remove('hidden')
+        document.querySelector('#my-business').classList.remove('hidden')
+    } else if (loggedInStatus.result === "Bad User Token") {
+        document.querySelector('#sign-up').classList.remove('hidden')
+        document.querySelector('#log-in').classList.remove('hidden')
+        document.querySelector('#profile').classList.add('hidden')
+        document.querySelector('#log-out').classList.add('hidden')
+        document.querySelector('#my-business').classList.add('hidden')
+    }
+
+    //logout event listener
+    document.querySelector('#log-out')
+        .addEventListener('click', () => {
+            localStorage.removeItem("HANGRY_ACCESS_TOKEN")
+            localStorage.removeItem("HANGRY_CURRENT_USER_ID")
+            window.location.href = '/';
+        })
+
+    document.getElementById('profile').addEventListener('click', async () => {
+        // window.location.href = `/`
+        window.location.href = `/user/${id}`
+    })
+
 
 
 
@@ -37,9 +98,22 @@ document.addEventListener('DOMContentLoaded', async () => {
             name, phone, city, state, address, foodCategory,
             dineIn, takeOut, delivery, userId, Reviews, Images
         } = restaurant;
-
+      // checking if the restaurant has reviews if it is not returning with 'No reviews yet' text
+            if (averageRating === 0) {
+                starsBar.innerHTML = "No reviews yet";
+            } else if (averageRating === 1) {
+                starsBar.innerHTML = `<span style='color:gold;'>${'<i class="fas fa-star"></i>'.repeat(1)}</span>`
+            } else if (averageRating === 2) {
+                starsBar.innerHTML = `<span style='color:gold;'>${'<i class="fas fa-star"></i>'.repeat(2)}</span>`
+            } else if (averageRating === 3) {
+                starsBar.innerHTML = `<span style='color:gold;'>${'<i class="fas fa-star"></i>'.repeat(3)}</span>`
+            } else if (averageRating === 4) {
+                starsBar.innerHTML = `<span style='color:gold;'>${'<i class="fas fa-star"></i>'.repeat(4)}</span>`
+            } else if (averageRating === 5) {
+                starsBar.innerHTML = `<span style='color:gold;'>${'<i class="fas fa-star"></i>'.repeat(5)}</span>`
+            }
         restaurantName.innerHTML = `<h2>${name}</h2>`;
-        starsBar.innerHTML = `Average rating: ${averageRating}`;
+        // starsBar.innerHTML = `Average rating: ${averageRating}`;
         reviewCount.innerHTML = `<p>${Reviews.length} reviews</p>`;
         foodCategories.innerHTML = `${foodCategory}`
         if (dineIn) {
