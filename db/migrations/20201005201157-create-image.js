af8eb9633564ae25b2c7c11c931be4a605dc145b
peter <southpeter77@gmail.com>
2020-10-11 15:59:57 -0400

    added pictures

--- db/migrations/20201005201157-create-image.js
+++ db/migrations/20201005201157-create-image.js
@@ -24,7 +24,7 @@ module.exports = {
       },
       userId: {
         type: Sequelize.INTEGER,
-        allowNull: false,
+        allowNull: true,
         references: {
           model: 'Users',
           key: 'id'
