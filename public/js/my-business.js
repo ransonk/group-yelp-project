e919b63f0ea8640ace17fdfdfc56694c4e1c41bf
Ranson Knorr <ransonknorr@gmail.com>
2020-10-11 15:11:41 -0500

    form styling added

--- public/js/my-business.js
+++ public/js/my-business.js
@@ -1,45 +1,124 @@
 import { handleErrors } from "./utils.js"
 
-const id = localStorage.getItem("HANGRY_CURRENT_USER_ID")
-const myBusinessForm = document.querySelector(".my-business-form")
-const myBusinessFormButton = document.querySelector(".my-business-sign-up-button")
-myBusinessFormButton.addEventListener("click", async (event) => {
-  event.preventDefault();
-  const formData = new FormData(myBusinessForm)
 
-  const name = formData.get("name")
-  const phone = formData.get("phone")
-  const city = formData.get("city")
-  const state = formData.get("state")
-  const address = formData.get("address")
-  const foodCategory = formData.get("foodCategory")
-  const dineIn = formData.get("dineIn")
-  const takeOut = formData.get("takeOut")
-  const delivery = formData.get("delivery")
-  const userId = id;
+document.addEventListener('DOMContentLoaded', async () => {
 
-  try {
+  const token = localStorage.getItem("HANGRY_ACCESS_TOKEN")
+  const id = localStorage.getItem("HANGRY_CURRENT_USER_ID")
 
-    const res = await fetch("/api/restaurants", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ name, phone, city, state, address, foodCategory, dineIn, takeOut, delivery, userId })
-    })
+  const body = { token, id }
+  const res = await fetch("/api/user/check", {
+    method: "POST",
+    body: JSON.stringify(body),
+    headers:
+      { "Content-Type": "application/json" },
+  })
 
-    if (!res.ok) {
-      throw res
+  const loggedInStatus = await res.json();
+
+  document.querySelector('#my-business').addEventListener('click', async (e) => {
+    const res = await fetch(`/api/restaurants/user/${id}/restaurant`);
+
+    const { restaurant } = await res.json();
+    if (!restaurant) {
+
+      window.location.href = "/my-business"
+      // console.log('need to create business page')
+    } else {
+
+      const restaurantId = restaurant.id;
+      window.location.href = `/restaurants/${restaurantId}`;
     }
-    const restaurants = await res.json()
-    const restaurantsId = restaurants.restaurant.id
+  })
 
-    window.location.href = `/restaurants/${restaurantsId}`
+  if (loggedInStatus.result === "Non-business Owner Token") {
+    document.querySelector('#sign-up').classList.add('hidden')
+    document.querySelector('#log-in').classList.add('hidden')
+    document.querySelector('#profile').classList.remove('hidden')
+    document.querySelector('#log-out').classList.remove('hidden')
+    document.querySelector('#my-business').classList.add('hidden')
+    document.querySelector('#demo-user').classList.add('hidden')
 
-  } catch (err) {
-    handleErrors(err)
+  } else if (loggedInStatus.result === "Business Owner Token") {
+    document.querySelector('#sign-up').classList.add('hidden')
+    document.querySelector('#log-in').classList.add('hidden')
+    document.querySelector('#profile').classList.remove('hidden')
+    document.querySelector('#log-out').classList.remove('hidden')
+    document.querySelector('#my-business').classList.remove('hidden')
+    document.querySelector('#demo-user').classList.add('hidden')
+
+  } else if (loggedInStatus.result === "Bad User Token") {
+    document.querySelector('#sign-up').classList.remove('hidden')
+    document.querySelector('#log-in').classList.remove('hidden')
+    document.querySelector('#profile').classList.add('hidden')
+    document.querySelector('#log-out').classList.add('hidden')
+    document.querySelector('#my-business').classList.add('hidden')
   }
 
+  //logout event listener
+  document.querySelector('#log-out')
+    .addEventListener('click', () => {
+      localStorage.removeItem("HANGRY_ACCESS_TOKEN")
+      localStorage.removeItem("HANGRY_CURRENT_USER_ID")
+      window.location.href = '/';
+    })
+
+  document.getElementById('profile').addEventListener('click', async () => {
+    // window.location.href = `/`
+    window.location.href = `/user/${id}`
+  })
+
+
+  //search event listner
+  const searchForm = document.querySelector(".search-form");
+  searchForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const formData = new FormData(searchForm)
+    const search = formData.get("search")
+    localStorage.setItem("searchValue", search)
+    window.location.href = `/search`
+  })
 
 
 
 
+  // const id = localStorage.getItem("HANGRY_CURRENT_USER_ID")
+  const myBusinessForm = document.querySelector(".my-business-form")
+  const myBusinessFormButton = document.querySelector(".my-business-sign-up-button")
+  myBusinessFormButton.addEventListener("click", async (event) => {
+    event.preventDefault();
+    const formData = new FormData(myBusinessForm)
+
+    const name = formData.get("name")
+    const phone = formData.get("phone")
+    const city = formData.get("city")
+    const state = formData.get("state")
+    const address = formData.get("address")
+    const foodCategory = formData.get("foodCategory")
+    const dineIn = formData.get("dineIn")
+    const takeOut = formData.get("takeOut")
+    const delivery = formData.get("delivery")
+    const userId = id;
+
+    try {
+
+      const res = await fetch("/api/restaurants", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ name, phone, city, state, address, foodCategory, dineIn, takeOut, delivery, userId })
+      })
+
+      if (!res.ok) {
+        throw res
+      }
+      const restaurants = await res.json()
+      const restaurantsId = restaurants.restaurant.id
+
+      window.location.href = `/restaurants/${restaurantsId}`
+
+    } catch (err) {
+      handleErrors(err)
+    }
+
+  })
 })
