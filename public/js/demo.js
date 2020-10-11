092f60e343682deae308a5cf5f6e38db53f02446
Ranson Knorr <ransonknorr@gmail.com>
2020-10-10 23:17:17 -0500

    navbar sticky now

--- public/js/demo.js
+++ public/js/demo.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", (event) => {
     /////////////////database must have a demo user and save the token and hard code in here //////////////
-    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoyMiwiZW1haWwiOiJkZW1vQGRlbW8uY29tIn0sImlhdCI6MTYwMjM4MzAzOCwiZXhwIjozMDAwMDAwMDE2MDIzODMwNDB9.owv7WTm0qFCqI1gfQFdx4QCxbbsMtz1x7D2viCLwmng"
-    const id = 22;
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoyOCwiZW1haWwiOiJkZW1vdXNlckBnbWFpbC5jb20ifSwiaWF0IjoxNjAyMzg4MDE0LCJleHAiOjMwMDAwMDAwMTYwMjM4ODAzMH0.6Ko0ue_10evIUg7PTTPmFeXv1JB3dQ2SOF6k462XGVU"
+    const id = 28;
 
     localStorage.setItem("HANGRY_ACCESS_TOKEN", token)
     localStorage.setItem("HANGRY_CURRENT_USER_ID", id)
@@ -19,20 +19,20 @@ document.addEventListener("DOMContentLoaded", (event) => {
     //     demoBubble.classList.add("fade-in")
     // })
 
-     firstButton.addEventListener("click", (event) => {
+    firstButton.addEventListener("click", (event) => {
 
         demoBubble.classList.add("fade-out")
         firstButton.classList.add("hidden")
-    setTimeout(function() {
-    demoBubble.classList.add("hidden")
-    secondButton.classList.add("fade-in")
-    secondButton.classList.remove("hidden")
-    demoBubble.classList.add("fade-in")
-    demoBubble.classList.remove("hidden")
-    demoBubble.classList.add("demo__bubble2")
-    },2000)
+        setTimeout(function () {
+            demoBubble.classList.add("hidden")
+            secondButton.classList.add("fade-in")
+            secondButton.classList.remove("hidden")
+            demoBubble.classList.add("fade-in")
+            demoBubble.classList.remove("hidden")
+            demoBubble.classList.add("demo__bubble2")
+        }, 2000)
         // demoBubble.classList.add("demo__bubble2")
- 
+
 
     })
 
