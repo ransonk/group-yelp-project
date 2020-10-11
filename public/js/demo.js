586a93f2b683db1aeec844d56c72a93624784247
Ranson Knorr <ransonknorr@gmail.com>
2020-10-10 21:17:56 -0500

    demo styling added to continue button

--- public/js/demo.js
+++ public/js/demo.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-/////////////////database must have a demo user and save the token and hard code in here //////////////
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoyMywiZW1haWwiOiJkZW1vVXNlckBkZW1vLmNvbSJ9LCJpYXQiOjE2MDIzMDAxNDEsImV4cCI6MzAwMDAwMDAxNjAyMzAwMTYwfQ.kEwQrc9WXQue6RQ50KiTna5OGDXNWpOXLJTb88ttwzc"
-const id = 23;
+    /////////////////database must have a demo user and save the token and hard code in here //////////////
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoyNiwiZW1haWwiOiJkZW1vQGdtYWlsLmNvbSJ9LCJpYXQiOjE2MDIzODExNDYsImV4cCI6MzAwMDAwMDAxNjAyMzgxMTAwfQ.5dGdnAwu-lxUHI_lHmKw35luKWQOuBH200oG_yn2F6Y"
+    const id = 26;
 
-localStorage.setItem("HANGRY_ACCESS_TOKEN", token)
-localStorage.setItem("HANGRY_CURRENT_USER_ID", id)
+    localStorage.setItem("HANGRY_ACCESS_TOKEN", token)
+    localStorage.setItem("HANGRY_CURRENT_USER_ID", id)
 
     const demoPicture = document.querySelector(".demo__picture");
     const demoBubble = document.querySelector(".demo__bubble");
@@ -16,8 +16,8 @@ localStorage.setItem("HANGRY_CURRENT_USER_ID", id)
         secondButton.classList.remove("hidden")
         demoBubble.classList.remove("demo__bubble")
         demoBubble.classList.add("demo__bubble2")
-   })
-   secondButton.addEventListener("click", (event) => {
-       window.location.href = '/'
-   })
+    })
+    secondButton.addEventListener("click", (event) => {
+        window.location.href = '/'
+    })
 })
